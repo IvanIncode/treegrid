9ef6bf6a97deddaab914067de05b10d8a1fca345
Ivan <ivan.r.incode.2021@gmail.com>
2022-01-04 17:24:32 +0200

    Add some functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
+import { ContextMenuModule } from '@syncfusion/ej2-angular-navigations';
 import { Component, ElementRef, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { dataSource, virtualData, textWrapData } from './datasource';
-import { VirtualScrollService, TreeGridComponent, ColumnMenuService, EditSettingsModel, ResizeService, EditService, ContextMenuService, Column } from '@syncfusion/ej2-angular-treegrid';
+import { VirtualScrollService, TreeGridComponent, ColumnMenuService, EditSettingsModel, ResizeService, EditService, ContextMenuService, Column, SelectionSettingsModel } from '@syncfusion/ej2-angular-treegrid';
 import { DialogComponent } from '@syncfusion/ej2-angular-popups';
 import { EmitType } from '@syncfusion/ej2-base';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ItemModel, MenuItemModel } from '@syncfusion/ej2-navigations';
-import { CancelEventArgs } from '@syncfusion/ej2-angular-inputs';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
 
   @ViewChild('treegrid') treegrid!: TreeGridComponent;
   @ViewChild('ejDialog') ejDialog!: DialogComponent;
+  @ViewChild('ejDialogDelete') ejDialogDelete!: DialogComponent;
   @ViewChild('container', { read: ElementRef, static: true }) container!: ElementRef;
   @ViewChild('newColumn') columnTemplate: Column;
   public data!: Object[];
@@ -27,9 +28,18 @@ export class AppComponent implements OnInit {
   public editing!: EditSettingsModel;
   public editparams!: Object;
   public targetElement!: HTMLElement;
+  public isDeleteConfirm: boolean;
   public hideDialog: EmitType<object> = () => {
     this.isConfirm = true;
     this.ejDialog.hide();
+  }
+  public hideDeleteDialog: EmitType<object> = () => {
+    this.isDeleteConfirm = true;
+    this.ejDialogDelete.hide();
+  }
+  public cancelDeleteDialog: EmitType<object> = () => {
+    this.isDeleteConfirm = false;
+    this.ejDialogDelete.hide();
   }
 
   public cancelDialog: EmitType<object> = () => {
@@ -41,17 +51,34 @@ export class AppComponent implements OnInit {
     {
       'click': this.hideDialog.bind(this),
         buttonModel:{
-        content: 'OK',
+        content: 'SAVE',
         isPrimary: true,
       }
     },
     {
       'click': this.cancelDialog.bind(this),
       buttonModel: {
-        content: 'Cancel'
+        content: 'CANCEL'
       }
     }
   ];
+
+  public deletionButtons: Object = [
+    {
+      'click': this.hideDeleteDialog.bind(this),
+        buttonModel:{
+        content: 'DELETE',
+      }
+    },
+    {
+      'click': this.cancelDeleteDialog.bind(this),
+      buttonModel: {
+        content: 'CANCEL',
+        isPrimary: true,
+      }
+    }
+  ];
+
   public items: ItemModel[] = [
     { text: 'string' },
     { text: 'boolean' },
@@ -86,8 +113,9 @@ export class AppComponent implements OnInit {
   public column5headerText: string = 'Column 5';
 
   public customAttributes: Object;
-
-  public newStyles: string;
+  public isHeaderClick: boolean;
+  public selectionOptions: SelectionSettingsModel = {type: 'Multiple'};
+  public selectedColumn: Column | null;
 
 
   constructor(private fb: FormBuilder){
@@ -108,10 +136,10 @@ export class AppComponent implements OnInit {
     dataSource();
     this.data = virtualData;
     this.contextMenuItems  = [
-      { text: 'Edit Column', id: 'EditCol' },
+      { text: 'Edit Column', id: 'EditCol'},
       { text: 'New Column', id: 'NewCol' },
       { text: 'Delete Column', id: 'DeleteCol' },
-      { text: 'Choose Colomn', id: 'ChooseCol' },
+      //{ text: 'Choose Colomn', id: 'ChooseCol' },
       { text: 'Freez Column', id: 'FreezCol' },
       { text: 'Filter Column',  id: 'FilterCol' },
       { text: 'MultiSort',  id: 'MultiSort' }
@@ -126,6 +154,15 @@ export class AppComponent implements OnInit {
   }
 
   contextMenuOpen(arg?: any) {
+    let isHeaderClick = false;
+    console.log(this.contextMenuItems.filter(item => item.id === 'FreezCol')[0])
+    document.querySelectorAll('div.e-headercelldiv').forEach((value, key) => {if(value === arg.rowInfo.target) isHeaderClick = true});
+    document.querySelectorAll('span.e-headertext').forEach((value, key) => {if(value === arg.rowInfo.target) isHeaderClick = true});
+    if(!isHeaderClick) {
+      arg.cancel = true;
+    } else {
+      arg.cancel = false;
+    };
     this.clearAllInputs();
     if(!arg.parentItem) {
       this.columnNameForManipulations = arg.column.field;
@@ -139,10 +176,16 @@ export class AppComponent implements OnInit {
   contextMenuClick(arg?: any) {
     if(arg) {
       this.actionToDo = arg.item.properties.id;
-      console.log(this.actionToDo)
+      //console.log(this.actionToDo)
+    }
+    if(this.actionToDo === 'EditCol' || this.actionToDo === 'NewCol' || this.actionToDo === 'DeleteCol') {
+      this.onOpenDialog(arg)
+    } else {
+      console.log(this.actionToDo, this.columnNameForManipulations)
+      const column = this.getColumn(this.columnNameForManipulations);
+      this.treegrid.openColumnChooser(0,0)
     }
 
-    this.onOpenDialog(arg)
   }
 
   checkAreInputsPristine(form: FormGroup) {
@@ -171,7 +214,11 @@ export class AppComponent implements OnInit {
     const column = this.getColumn(this.columnNameForManipulations)
     this.prepareInputsForAddingOrEditing(column)
 
-    this.ejDialog.show();
+    if(this.actionToDo === 'EditCol' || this.actionToDo === 'NewCol') {
+      this.ejDialog.show();
+    } else if(this.actionToDo === 'DeleteCol') {
+      this.ejDialogDelete.show();
+    }
   }
 
   prepareInputsForAddingOrEditing(column: any) {
@@ -194,9 +241,22 @@ export class AppComponent implements OnInit {
   closeDialog(arg?: any) {
     this.addOrEditForm.updateValueAndValidity();
     if (this.isConfirm && this.addOrEditForm.status === 'VALID') {
-      this.whatNeedToDoAndDoIt(this.columnNameForManipulations);
+      //this.whatNeedToDoAndDoIt(this.columnNameForManipulations);
     } else if(this.isConfirm && this.addOrEditForm.status !== 'VALID') {
       alert('wrong form values, please check data!')
+    }
+  }
+
+  closeDeleteDialog(event: any) {
+    if(this.isDeleteConfirm) {
+      const column = this.getColumn(this.columnNameForManipulations);
+      const oldColumns = this.treegrid.columns as Column[];
+      let idx = -1;
+      for(let i = 0; i < oldColumns.length; i++) {
+        if(oldColumns[i] === column) idx = i;
+      }
+      oldColumns.splice(idx, 1);
+      this.treegrid.refreshColumns();
     }
   }
 
@@ -214,11 +274,9 @@ export class AppComponent implements OnInit {
         this.manipulationsWithInputs(fieldValuesPairs, column);
         break;
       case 'NewCol':
-        const colIndex = column.uid;
-        const colHeader = document.querySelector<HTMLElement>('#_gridcontrolcolgroup');
-        colHeader?.appendChild(colHeader?.firstChild as Node)
-        this.treegrid.refreshColumns
-        //console.log(colHeader);
+        this.addNewColumn(column);
+        break;
+      case 'DeleteCol':
         break;
     }
     this.treegrid.refreshColumns();
@@ -226,7 +284,8 @@ export class AppComponent implements OnInit {
 
   manipulationsWithInputs(fieldValuesPairs: any, column: Column) {
     for(let [input, value] of fieldValuesPairs) {
-      switch(input) {
+      this.switcCaseInput(input, column, value);
+      /* switch(input) {
         case 'columnName':
           column.headerText = value;
           break;
@@ -254,9 +313,65 @@ export class AppComponent implements OnInit {
         case 'columnTextWrap':
           column.customAttributes = {style: {...(column.customAttributes['style']), 'white-space': value, }}
           break;
-      }
+      } */
     }
     this.treegrid.refreshColumns();
+  }
+
+  switcCaseInput(input: string, column: Column, value: any) {
+    switch(input) {
+      case 'columnName':
+        column.headerText = value;
+        break;
+      case 'columnDataType':
+        column.type = value;
+        break;
+      case 'columnDefaultValue':
+        column.defaultValue = value;
+        break;
+      case 'columnMinWidth':
+        column.minWidth = value;
+        break;
+      case 'columnFontSize':
+        column.customAttributes = {style: {...(column.customAttributes['style']), 'font-size': `${value}px`, }}
+        break;
+      case 'columnFontColor':
+        column.customAttributes = {style: {...(column.customAttributes['style']), 'color': value, }}
+        break;
+      case 'columnBackgroundColor':
+        column.customAttributes = {style: {...(column.customAttributes['style']), 'background-color': value, }}
+        break;
+      case 'columnTextAligh':
+        column.textAlign = value;
+        break;
+      case 'columnTextWrap':
+        column.customAttributes = {style: {...(column.customAttributes['style']), 'white-space': value, }}
+        break;
+    }
+  }
+
+  addNewColumn(column: Column) {
+    const oldColumns = this.treegrid.columns as Column[];
+    let idx = -1;
+    for(let i = 0; i < oldColumns.length; i++) {
+      if(oldColumns[i] === column) idx = i + 1;
+    }
+    const inputs = this.checkAreInputsPristine(this.addOrEditForm); // Map
+    const newFieldIdx = +oldColumns[oldColumns.length - 1].field.replace('FIELD','') + 1;
+    const newColumn = <Column> {
+      field: 'FIELD' + newFieldIdx,
+      headerText: inputs!.has('columnName') ? inputs!.get('columnName') : 'Column...',
+      width: 150,
+      type: inputs!.has('columnDataType') ? inputs!.get('columnDataType') : 'string',
+      defaultValue: inputs!.has('columnDefaultValue') ? inputs!.get('columnDefaultValue') : 'value' || true || 0 || null,
+      minWidth: inputs!.has('columnName') ? inputs!.get('columnName') : 150,
+      textAlign: inputs!.has('columnTextAligh') ? inputs!.get('columnTextAligh') : 'Center',
+    }
+    if(!newColumn.customAttributes) newColumn.customAttributes = {style: {}};
+    if(inputs!.has('columnFontSize')) newColumn.customAttributes = {style: {...(newColumn.customAttributes['style']), 'font-size': `${inputs!.get('columnFontSize')}px`, }};
+    if(inputs!.has('columnFontColor')) newColumn.customAttributes = {style: {...(newColumn.customAttributes['style']), 'color': `${inputs!.get('columnFontColor')}`, }};
+    if(inputs!.has('columnBackgroundColor')) newColumn.customAttributes = {style: {...(newColumn.customAttributes['style']), 'background-color': `${inputs!.get('columnBackgroundColor')}`, }};
+    oldColumns.splice(idx, 0, newColumn);
   }
 
   clearAllInputs() {
@@ -279,8 +394,15 @@ export class AppComponent implements OnInit {
     this.choosedDataType = event.element.outerText;
   }
 
-  onSubmit() {
-
-  }
+/*   click(e: any) {
+    let c = <Column[]>
+        [
+          { field: 'ShipCountry', headerText: 'Ship Country', width: 150 }
+        ];
+        for( let i: number = 0; i < c.length; i++ ) {
+    (this.treegrid.columns as Column[]).push( c[ i ] );
+    this.treegrid.refreshColumns();
+    }
+  } */
 
 }
